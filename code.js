4f0dc64e6786a73ec01d0242be80967e29bfe3a6
MonFlix <119066950+MonFlix@users.noreply.github.com>
2022-11-27 12:38:55 +0000

    Add files via upload

--- code.js
+++ code.js
@@ -1,15 +1,15 @@
 
 const persons = [
 
-    {name:'Manifest (Lien de la page)'},
+    {name:'Manifest (https://mon-flix.tk/manifest.html)'},
     {name:'La Vie Scolaire (https://uqload.org/wfpmo6qahho7.html)'},
     {name: 'BlackPanther (https://uqload.org/q07ao200eds3.html)'},
     {name:'BlackPanther Wakanda For Ever (https://uqload.org/ej2yyguqw83i.html)'},
     {name:'Athena (https://uqload.org/xopjmbgwnc1t.html)'},
-    {name:'Dahmer (Lien de la page)'},
+    {name:'Dahmer (https://mon-flix.tk/dahmer.html)'},
     {name:'Black Adam (https://sblongvu.com/e/czqd9p0hkvu0)'},
-    {name:'Wednesday (Lien de la page)'},
-    {name:'Mercredi (Lien de la page)'}
+    {name:'Wednesday (https://mon-flix.tk/wednesday.html)'},
+    {name:'Mercredi (https://mon-flix.tk/wednesday.html)'}
 ];
 
 const searchinput = document.getElementById('searchInput');
